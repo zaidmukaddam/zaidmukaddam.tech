7918d1c9e236e6072905a21dbff2864de0cbe592
zaidmukaddam <zaidaltafmukaddam@gmail.com>
2022-02-07 15:08:06 +0530

    feat: gradient borders added to tech-cards

--- components/pages/home/tech-stack-card.tsx
+++ components/pages/home/tech-stack-card.tsx
@@ -8,8 +8,8 @@ export type CardProps = {
 
 const TechStackCard = ({ label, img,techUrl }: CardProps) => {
   return (
-    <div className='h-[100px] border-4 cursor-pointer hover:scale-105 duration-150 transition border-violet-600 dark:border-indigo-500 rounded-lg p-2'>
-      <div className='flex flex-row items-center h-full px-4 space-x-2'>
+    <div className='h-[100px] cursor-pointer hover:scale-105 duration-150 transition b rounded-lg p-1.5 bg-gradient-to-r from-indigo-500 to-blue-400'>
+      <div className='flex flex-row items-center bg-[#f5f5f5] dark:bg-brand-black rounded-lg h-full px-4 space-x-2'>
         <div className='w-full lg:w-1/2 flex justify-center'>
           <a href={techUrl}>
             <Image src={img as string} width='54' height='54' alt={label} />
--- components/pages/home/tech-stack.tsx
+++ components/pages/home/tech-stack.tsx
@@ -11,7 +11,7 @@ const TechStack = ({ dataTechStacks }: TechStackProps) => {
       <div className='px-8 xl:px-0 pt-4 xl:w-[68rem] mx-auto'>
         <div className='flex flex-col'>
           <h2 className='text-4xl font-bold'>Current Tech Stack</h2>
-          <p className='text-[#525252] mt-4'>
+          <p className='text-stone-400 mt-4'>
             Here is my tech stack I use to develop amazing websites and projects!
           </p>
         </div>
